b61c01c43d5c05d9db697f59008f095b3c6e02f0
Thomas McKee <caffeine.storm@gmail.com>
2026-02-20 09:46:58 -0500

    delegate to 'cabal test' but fall back to 'make test' if needed

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ all: readme.html
 
 readme.html: README.md
 	md2html $^ > $@
+
+.PHONY: test
+test:
+	cabal test
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,63 @@
 module Main where
 
+import qualified Data.List
+import qualified Data.Maybe
+import qualified System.Directory
+import qualified System.Process
+
+
+data TestStrategy = CabalTest | MakeTest
+  deriving(Show, Eq)
+
+dirEntToTestStrategy :: String -> Maybe TestStrategy
+dirEntToTestStrategy "Makefile" = Just MakeTest
+dirEntToTestStrategy ent | ".cabal" `Data.List.isSuffixOf` ent = Just CabalTest
+                         | otherwise = Nothing
+
+
+type TestRunner = IO ()
+
+prepareTestRunner :: TestStrategy -> TestRunner
+prepareTestRunner CabalTest = runCabalTest
+prepareTestRunner MakeTest = runMakeTest
+
+runCabalTest :: TestRunner
+runCabalTest = System.Process.callCommand "cabal test"
+
+runMakeTest :: TestRunner
+runMakeTest = System.Process.callCommand "make test"
+
+noRunnerFound :: TestRunner
+noRunnerFound = putStrLn "tatt: couldn't detect test runner"
+
+
+data Prefs = StubPrefs
+
+determinePrefs :: IO Prefs
+determinePrefs = return StubPrefs
+
+betterStrat :: Prefs -> TestStrategy -> TestStrategy -> TestStrategy
+betterStrat StubPrefs lhs rhs =
+  case (lhs, rhs) of
+    (CabalTest,_) -> CabalTest
+    (_,CabalTest) -> CabalTest
+    _ -> MakeTest
+
+bestStrat :: Prefs -> [TestStrategy] -> TestStrategy
+bestStrat _ [] = error "can't pick best from no choices"
+bestStrat _ [it] = it
+bestStrat p lst = foldr1 (betterStrat p) lst
+
+pickTestStrategy :: Prefs -> [String] -> TestRunner
+pickTestStrategy prefs dirlist =
+  let possibleStrats = Data.Maybe.mapMaybe dirEntToTestStrategy dirlist
+  in if null possibleStrats
+    then noRunnerFound
+    else prepareTestRunner $ bestStrat prefs possibleStrats
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  prefs <- determinePrefs
+  dirlist <- System.Directory.listDirectory "."
+  pickTestStrategy prefs dirlist
